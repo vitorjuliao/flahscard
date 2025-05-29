5d40f1be049b5e0de14c737122b09ebb2591a07a
vitorjuliao <victor.juliao.silva@escola.pr.gov.br>
2025-05-29 15:20:41 -0300

    Add files via upload

---
+++ tiago-3t-2b-main/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Aritmética',
+    'Quais são as operações básicas da Matemática?',
+    'Adição, Subtração, Multiplicação e Divisão.'
+)
+
+criaCartao(
+    'Função',
+    'O que seria o zero da função?',
+    'O valor onde o gráfico cruza o eixo da abscissa, ou ainda o valor de x para quando y for igual a 0.'
+)
+
+criaCartao(
+    'Estatística',
+    'Qual a definição de média aritmética?',
+    'Resultado da divisão do somatório dos números dados pela quantidade de números somados.'
+)
+
+criaCartao(
+    'Cálculo',
+    'Qual o valor de 58 - 2 x 10?',
+    '38.'
+)
+
+criaCartao(
+    'Razões Trigonométricas',
+    'Qual o valor de cosseno de 60º?',
+    '1/2.'
+)
+
+criaCartao(
+    'Ordem das Operações',
+    'Qual a ordem das operações matemática, da com maior para a menor prioridade?',
+    'Parênteses, Expoentes, Multiplicação e Divisão, Adição e Subtração.'
+)
